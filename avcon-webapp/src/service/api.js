52b5799bcd766fbff3d5c3370cc6209e68e21dae
Pablo Woinarovicz Ramos <pablowoina2205@gmail.com>
2021-09-12 18:40:28 -0300

    baseurl config

--- avcon-webapp/src/service/api.js
+++ avcon-webapp/src/service/api.js
@@ -1,10 +1,9 @@
 import axios from 'axios';
 
-//const BASE_URL = process.env.REACT_APP_BASE_URL;
+export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'http://127.0.0.1:5000';
 
 const api = axios.create({
-    //baseURL: `${BASE_URL}/`
-    baseURL: "http://127.0.0.1:5000/"
+    baseURL: `${BASE_URL}/`
 });
 
 export default api;
