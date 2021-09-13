b2245a0f629a770b0da175ed818694d067cde7e6
Pablo Woinarovicz Ramos <pablowoina2205@gmail.com>
2021-09-12 21:36:51 -0300

    service deploy config

--- avcon-service/app.py
+++ avcon-service/app.py
@@ -13,6 +13,11 @@ app.config['UPLOAD_FOLDER'] = ut.get_temp_path()
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1000 * 1000
 
 
+@app.route('/')
+def index():
+    return "<h1>Hello World</h1>"
+
+
 @app.route('/convert', methods=['POST'])
 def convert():
     if request.method == 'POST':
@@ -20,5 +25,11 @@ def convert():
         cc.execute()
         return cc.get_response()
 
+'''
+if __name__ == '__main__':
+    app.run()
+'''
 
-app.run()
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
